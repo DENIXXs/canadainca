4569d9bf65ac6a61a3e35c1b1861784c8afab6c5
Denis Safronov <safron_dz@mail.ru>
2020-04-14 22:03:38 +0300

    Add 'edit' and 'destroy'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ def create
 	@post = Post.new(post_params)
 
 	if @post.save
-		redirec_to @post
+		redirect_to post_path(@post)
 	else
 		render 'new'
 	end
@@ -19,6 +19,28 @@ def create
 end
 def show
 	@post = Post.find(params[:id])	
+end
+
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	@post = Post.find(params[:id])
+
+	if @post.update(post_params)
+		redirect_to @post
+	else
+		render 'edit'
+	end
+end
+
+def destroy
+	@post = Post.find(params[:id])
+
+	@post.delete
+	redirect_to posts_path
+	
 end
 
 private
