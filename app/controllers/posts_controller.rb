3b3899ff708fd8e9c72798cd8ee823b796f1f197
Denis Safronov <safron_dz@mail.ru>
2020-04-16 09:03:00 +0300

    Edit controller post and view '_form'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+
+	before_action :set_post, only: [:show, :edit, :update, :destroy] 
+
 def index
 	@posts = Post.all
 end 
 
 def new
-
+	@post = Post.new
 end
 
 def create
@@ -17,17 +20,17 @@ def create
 	end
 
 end
+
 def show
-	@post = Post.find(params[:id])	
+	
 end
 
 def edit
-	@post = Post.find(params[:id])
+	
 end
 
 def update
-	@post = Post.find(params[:id])
-
+	
 	if @post.update(post_params)
 		redirect_to @post
 	else
@@ -36,16 +39,17 @@ def update
 end
 
 def destroy
-	@post = Post.find(params[:id])
-
-	@post.delete
+	@post.destroy
 	redirect_to posts_path
-	
 end
 
 private
 def post_params
 	params.require(:post).permit(:name, :body)
+end
+
+def set_post
+	@post = Post.find(params[:id])
 end
 	
 end
